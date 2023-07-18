ffd4f8c03a74771390ce04a00e29d34acb4e474b
Maria <m.tur67@gmail.com>
2023-07-18 17:26:56 +0300

    modified seminar5

--- seminar5/seminar5.sql
+++ seminar5/seminar5.sql
@@ -2,19 +2,21 @@ USE seminar5;
 
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-username VARCHAR(50) PRIMARY KEY,
-password VARCHAR(50) NOT NULL,
-status VARCHAR(10) NOT NULL
+CREATE TABLE users 
+(
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    status VARCHAR(10) NOT NULL
 );
 
 DROP TABLE IF EXISTS users_profile;
-CREATE TABLE users_profile (
-username VARCHAR(50) PRIMARY KEY,
-name VARCHAR(50) NOT NULL,
-address VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL,
-FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+CREATE TABLE users_profile 
+(
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
 );
 
 INSERT INTO users values
